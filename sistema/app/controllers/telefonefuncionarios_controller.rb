dec26d9d659b3b0a0fd0a263e611cb6513fe8fe6
driellylm <marques.drielly@gmail.com>
2014-03-24 14:26:47 -0300

    Gerenciar Telefone Funcionário

--- sistema/app/controllers/telefonefuncionarios_controller.rb
+++ sistema/app/controllers/telefonefuncionarios_controller.rb
@@ -1,2 +1,7 @@
 class TelefonefuncionariosController < InheritedResources::Base
+
+    def build_resource_params
+        		[params.fetch(:telefonefuncionario, {}).permit(:numerotelefone, :funcionario_id)]
+    	end
+
 end
